72f8e7e271f7dca98f1944932ffc7ae18ff89bb2
kunlun1988 <kunlun1988@me.com>
2021-03-18 10:57:36 -0500

    add sn function

--- Py_card_deck.py
+++ Py_card_deck.py
@@ -1,2 +1,11 @@
 ## this is use for practice in python and have more fun
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 print('hello world')
+y = np.linspace(0,1,100)
+x = sin(y)
+print(x)
+
+# add a sin function
